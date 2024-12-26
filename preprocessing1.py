4cf6c21badfb6fd9c2126f0c569880dbba4d7670
Aniketkr63 <154797309+Aniketkr63@users.noreply.github.com>
2024-12-26 14:11:04 +0530

    Update preprocessing1.py

--- preprocessing1.py
+++ preprocessing1.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-# Author: Antoine DELPLACE
-# Last update: 17/01/2020
+# Author: Aniket Singh
 """
 Pre-processing program to extract window-related data from Netflow files
 
